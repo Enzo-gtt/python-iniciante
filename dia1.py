fdc86cf616880f3ec22652deef707156e9d78875
Enzo-gtt <enzogoncalvescordeirodelima@gmail.com>
2026-02-26 00:26:10 -0300

    Update dia1.py

--- dia1.py
+++ dia1.py
@@ -3,3 +3,8 @@ print(f"Ola {cliente}, estamos com uma taxa de R$10 por produto.")
 
 produto = float(input("Qual o preço do produto? "))
 print(f"O novo preço é R${produto + 10:.2f}")
+
+from datetime import datetime
+
+agora = datetime.now()
+print(f"{agora.day}/{agora.month}/{agora.year}")
