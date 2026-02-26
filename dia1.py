aa8febf322d5c1773847311263278f886800c86c
Enzo-gtt <enzogoncalvescordeirodelima@gmail.com>
2026-02-26 00:00:49 -0300

    Create dia1.py

---
+++ dia1.py
@@ -0,0 +1,5 @@
+cliente = input("qual seu nome? ")
+print(f"Ola {cliente}, estamos com uma taxa de R$10 por produto.")
+
+produto = float(input("Qual o preço do produto? "))
+print(f"O novo preço é R${produto + 10:.2f}")
